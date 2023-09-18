e726f0182d8c9db5d54cde58e4ce462bbae11e19
bongdaring <chlwlsdud258@gmail.com>
2023-09-18 16:12:56 +0900

    mybatis finish

--- mysite03/src/main/java/com/poscodx/mysite/repository/GuestBookRepository.java
+++ mysite03/src/main/java/com/poscodx/mysite/repository/GuestBookRepository.java
@@ -1,17 +1,9 @@
 package com.poscodx.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +14,6 @@ import com.poscodx.mysite.vo.GuestBookVo;
 @Repository
 public class GuestBookRepository {
 	@Autowired
-	private DataSource dataSource;
-	@Autowired
 	private SqlSession session;
 	
 	public List<GuestBookVo> findAll() {
@@ -33,128 +23,23 @@ public class GuestBookRepository {
 		return result;
 	}
 	
-	public void insert(GuestBookVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
+	public boolean insert(GuestBookVo vo) {
+		int count = session.insert("guestbook.insert", vo);
 		
-		try {
-			conn = dataSource.getConnection();
-			
-			String sql =
-					"insert into guestbook values(null, ?, ?, ?, now());";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getContents());
-			
-			pstmt.executeQuery();
-			
-		} catch (SQLException e) {
-			System.out.println("error:"+e);
-		} finally {
-			// 6. 자원정리
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		return count==1;
 	}
 	
 	
 	public GuestBookVo findByNo(int no) {
-		GuestBookVo vo = null;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {	
-			conn = dataSource.getConnection();
-			
-			String sql =
-					"select password from guestbook where no=?";
-			pstmt = conn.prepareStatement(sql);	
-			pstmt.setInt(1, no);
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				String password = rs.getString(1);
-				
-				vo = new GuestBookVo();
-				vo.setPassword(password);
-			}		
-		} catch (SQLException e) {
-			System.out.println("error:"+e);
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return vo;
+		return session.selectOne("guestbook.findByNo", no);
 	}
 	
-	public void deleteByNo(int no) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
+	public boolean deleteByNo(int no) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("no", no);
 		
-		try {
-			conn = dataSource.getConnection();
-			
-			String sql =
-					"delete from guestbook where no=?";
-			pstmt = conn.prepareStatement(sql);		
-			pstmt.setInt(1, no);
-			
-			pstmt.executeQuery();
-			
-		} catch (SQLException e) {
-			System.out.println("error:"+e);
-		} finally {
-			// 6. 자원정리
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		
+		int count = session.delete("guestbook.deleteByNo", map);
+		return count == 1;
 	}
-	
-	
-//	
-//	private Connection getConnection() throws SQLException {
-//		Connection conn = null;
-//		try {
-//			Class.forName("org.mariadb.jdbc.Driver");
-//			String url = "jdbc:mariadb://192.168.0.173:3307/webdb?charset=utf8";
-//			conn = DriverManager.getConnection(url, "webdb", "mysql123");
-//		} catch (ClassNotFoundException e) {
-//			System.out.println("드라이버 로딩 실패 : "+e);
-//		}
-//
-//		return conn;
-//	}
+
 }
